894632a838fe8fd9c2a956ef684456954bc23792
alfredolopez80 <alfredolopez80@gmail.com>
2021-03-08 14:56:02 -0400

    FIX: Changes in AddFrozenWallet, add _transfer Method ERC Standard

--- truffle-config.js
+++ truffle-config.js
@@ -29,9 +29,9 @@ module.exports = {
       network_id: 4,
       gas: 8000000,
       gasPrice: 18000000000,
-      timeoutBlocks: 5000000,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 5000000,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
