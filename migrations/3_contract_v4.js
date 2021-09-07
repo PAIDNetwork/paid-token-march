62fc57d44d72fb3b9d6650eac0f26188a09bab43
Ruben Guevara <60409130+rubengue@users.noreply.github.com>
2021-09-07 14:14:58 -0500

    Update 3_contract_v4.js

--- migrations/3_contract_v4.js
+++ migrations/3_contract_v4.js
@@ -1,1 +1,19 @@
+const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 
+const PaidTokenBSCV4 = artifacts.require("PaidTokenBSCV4");
+const PaidTokenV4 = artifacts.require("PaidTokenV4");
+
+const EXISTING_BSCTESTNET_CONTRACT_ADDRESS =
+  "0xe1de1dc4de074e9c8bbf5e2d66cfdb4f0b2cb61a"; //there has to be updated this address, since this one is from Rinkeby 
+
+module.exports = async function(deployer, network) {
+  let oldAdress = EXISTING_BSCTESTNET_CONTRACT_ADDRESS;
+  if (network != "bsctestnet") {
+    oldAdress = await PaidTokenV4.address;
+  }
+
+  const instance = await upgradeProxy(oldAdress, PaidTokenBSCV4, {
+    deployer,
+  });
+  console.log("Upgraded", instance.address);
+};
