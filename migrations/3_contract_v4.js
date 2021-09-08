8da0cfbb8ed32d7b8c1e6e9e60e82f424989f9b4
Ruben Guevara <60409130+rubengue@users.noreply.github.com>
2021-09-08 09:35:46 -0500

    Update 3_contract_v4.js

--- migrations/3_contract_v4.js
+++ migrations/3_contract_v4.js
@@ -4,15 +4,15 @@ const PaidTokenBSCV4 = artifacts.require("PaidTokenBSCV4");
 const PaidTokenV4 = artifacts.require("PaidTokenV4");
 
 const EXISTING_BSCTESTNET_CONTRACT_ADDRESS =
-  "0xe1de1dc4de074e9c8bbf5e2d66cfdb4f0b2cb61a"; //there has to be updated this address, since this one is from Rinkeby 
+  "0xb2Db221f11C4Ca84682ce17ff0058feBB79C89BE"; 
 
 module.exports = async function(deployer, network) {
-  let oldAdress = EXISTING_BSCTESTNET_CONTRACT_ADDRESS;
+  let oldAddress = EXISTING_BSCTESTNET_CONTRACT_ADDRESS;
   if (network != "bsctestnet") {
-    oldAdress = await PaidTokenV4.address;
+    oldAddress = await PaidTokenV4.address;
   }
 
-  const instance = await upgradeProxy(oldAdress, PaidTokenBSCV4, {
+  const instance = await upgradeProxy(oldAddress, PaidTokenBSCV4, {
     deployer,
   });
   console.log("Upgraded", instance.address);
